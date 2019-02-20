3a6e7692c9e617bec1f237bd4386296ee08be152
Brian Holbrook <bpholbrook12@gmail.com>
2019-02-20 12:52:30 -0600

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Hues from './containers/Hues'
+import Footer from './components/Footer'
 
 export default () => {
   return (
     <React.Fragment>
       <Header />
       <Hues />
+      <Footer />
     </React.Fragment>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+// Footer component
+import React from 'react'
+import './index.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <span>
+            <a
+              href='https://github.com/bpholbrook/hue'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              View source on GitHub
+            </a>
+          </span>
+        </li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
