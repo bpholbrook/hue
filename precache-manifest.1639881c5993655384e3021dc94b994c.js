b1d15a4a044c13be258163ac88d910b55285f543
Brian Holbrook <bpholbrook12@gmail.com>
2019-02-19 20:23:50 -0600

    Updates

---
+++ precache-manifest.1639881c5993655384e3021dc94b994c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "454ff25d313323481f13",
+    "url": "/hue/static/js/runtime~main.454ff25d.js"
+  },
+  {
+    "revision": "59138cbbf3aa65089aa4",
+    "url": "/hue/static/js/main.59138cbb.chunk.js"
+  },
+  {
+    "revision": "0d41f906bf47efbfd46b",
+    "url": "/hue/static/js/2.0d41f906.chunk.js"
+  },
+  {
+    "revision": "59138cbbf3aa65089aa4",
+    "url": "/hue/static/css/main.db906528.chunk.css"
+  },
+  {
+    "revision": "8242c77d40f68756d8d94c3aa5b32aa4",
+    "url": "/hue/index.html"
+  }
+];
