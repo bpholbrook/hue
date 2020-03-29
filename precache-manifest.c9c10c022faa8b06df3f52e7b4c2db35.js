a47f3b01e4ac19c08e68b1925f355a51464bb278
Brian Holbrook <bpholbrook12@gmail.com>
2020-03-29 17:02:15 -0500

    Updates

---
+++ precache-manifest.c9c10c022faa8b06df3f52e7b4c2db35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e93f1d9c421d8e012293a925b0df65",
+    "url": "/hue/index.html"
+  },
+  {
+    "revision": "aadd27b059ff6017d03d",
+    "url": "/hue/static/css/main.a6e75273.chunk.css"
+  },
+  {
+    "revision": "1f208b525ebfa1b124e4",
+    "url": "/hue/static/js/2.87de6ad2.chunk.js"
+  },
+  {
+    "revision": "aadd27b059ff6017d03d",
+    "url": "/hue/static/js/main.d19442ca.chunk.js"
+  },
+  {
+    "revision": "ad92e99512a161f1831b",
+    "url": "/hue/static/js/runtime~main.0c88a89a.js"
+  }
+]);
